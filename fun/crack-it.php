20b9e3006b5cb659288ca3a1841c02c2966383b3
arshidkv12 <arshidkv12@gmail.com>
2019-07-19 19:03:57 +0530

    init

---
+++ fun/crack-it.php
@@ -0,0 +1,2 @@
+<?php bolt_decrypt( __FILE__ , '123abc'); return 0;
+    ##!!!##+/rc+PssJCzcxsYzJCUoIeQwLjEh5TfG3Nzc3CEfJCvc4w8wHS4w3N3d4/fG3Nzc3B4uIR0n98Y5xsYhHyQr3OP/Lh0fJ9wqKzPc6dwGMS8w3CIrLtwiMSrj9w==
